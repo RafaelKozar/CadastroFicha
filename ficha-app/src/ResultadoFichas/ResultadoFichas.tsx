f265d6251505e0ae34bb29585f7ed6006169a600
Rafael Kozar <rafael.kozar@alta-rt.com>
2022-04-01 11:52:05 -0300

    Result show calculations

--- ficha-app/src/ResultadoFichas/ResultadoFichas.tsx
+++ ficha-app/src/ResultadoFichas/ResultadoFichas.tsx
@@ -1,36 +1,65 @@
 import { observer } from "mobx-react-lite"
-import { useEffect, useState } from "react"
+import { useCallback, useEffect, useState } from "react"
 import { Button, Card, Statistic, Progress } from "semantic-ui-react";
 import agent from "../api/agent";
 import { TotalFichas } from "../models/totalFichas";
 import { useStore } from "../stores/store";
 import styles from './styles.module.scss';
 import Resultado from '../resultadoCadastro/ResultadoCadastro';
+import { Exception } from "sass";
 
 export default observer(function ResultadoFichas() {
     const { modalStore } = useStore();
-    const [totalFichas, setTotalFichas] = useState<TotalFichas>();
+    const [totalFichas, setTotalFichas] = useState<TotalFichas>({ peoples: 0, total: 0 });
     const [porcentagemAlcancada, setPorcentagemAlcancada] = useState<number>(0);
+    const [teste, setT] = useState<Number>()
     const [objetivo, setObjetivo] = useState<number>(100);
 
-    useEffect(() => {
-        agent.FichaApi.getQuantity().then((r : TotalFichas) => {
-            r.total = 80
-            setTotalFichas(r)
+    
 
-            var porcento = 0;
-            if(totalFichas?.total)
-                 porcento = totalFichas!.total/objetivo *100;
-            while (porcento > 95) {
-                setObjetivo(objetivo*1.5)
-                porcento = totalFichas!.total/objetivo *100;
+    useEffect(() => {        
+
+        var objetivoCounter = objetivo
+        var porcentoCalculado = 0
+        agent.FichaApi.getQuantity().then((r : TotalFichas) => {
+            r.total = 150                                   
+            setTotalFichas(r)     
+
+            porcentoCalculado = (r.total/objetivo) *100;
+            while (porcentoCalculado > 98) {
+                objetivoCounter = Math.round(objetivoCounter* 1.2)
+                porcentoCalculado = Math.round((r!.total/objetivoCounter) *100);
             }
-            setPorcentagemAlcancada(porcento)
+            setPorcentagemAlcancada(porcentoCalculado)
+            setObjetivo(objetivoCounter)
 
         }).catch((r) => {
             modalStore.openModal(<Resultado sucesso={false} messageError={r} />, false);
         })
+
+        // const getQuantiade = async () => {
+        //     try {
+        //         const r: TotalFichas = await agent.FichaApi.getQuantity();
+        //         r.total = 95
+        //         setTotalFichas(r)
+        //         setT(r.total)
+        //         setPorcentagemAlcancada((totalFichas!.total / objetivo) * 100);
+        //         while (porcentagemAlcancada > 95) {
+        //             setObjetivo(objetivo * 1.5)
+        //             setPorcentagemAlcancada((totalFichas!.total / objetivo) * 100);
+        //         }
+        //     }
+        //     catch(e ){
+        //         console.log(e) 
+        //         modalStore.openModal(<Resultado sucesso={false} messageError="Problema na hora de carregar o serviço" />, false);
+        //     }
+
+        // }
+        // getQuantiade()                
+
     }, [])
+
+    
 
     return (
         <div className={styles.containerMain}>
