8467e6d106422ef71138e6daabe833fc1f9ec06c
Rafael Kozar <rafael.kozar@alta-rt.com>
2022-04-10 13:46:52 -0300

    remove semantic ui from modal

--- ficha-app/src/modalContainer/ModalContainer.tsx
+++ ficha-app/src/modalContainer/ModalContainer.tsx
@@ -1,21 +1,46 @@
 import { observer } from "mobx-react-lite"
-import { Button, Modal, ModalActions } from "semantic-ui-react"
+import { Button as Tt, Modal as Tta, ModalActions } from "semantic-ui-react"
+
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+    Button,
+    useDisclosure
+} from '@chakra-ui/react'
+
 import { useStore } from "../stores/store";
+import { useEffect } from "react";
 
 
 export default observer(function ModalContainer() {
-    const { modalStore,  } = useStore();
+    const { modalStore } = useStore();
+    const {onOpen, isOpen } = useDisclosure()
+
+    useEffect(() => {        
+        onOpen();
+    }, [])
+
+    
     return (
-        <Modal open={modalStore.modal.open} onClose={modalStore.closeModal} >
-            <Modal.Header>
+        <Modal isOpen={isOpen} onClose={modalStore.closeModal} size='lg'>
+            {/* <ModalOverlay
+                bg='blackAlpha.300'
+                backdropFilter='blur(10px) hue-rotate(90deg)'
+            /> */}
+            <ModalHeader>
                 {modalStore.sucesso ? "Cadastro Realizado com sucesso" : "Ops, aconteceu algum problema"}
-            </Modal.Header>
-            <Modal.Content>
+            </ModalHeader>
+            <ModalBody>
                 {modalStore.modal.body}
-            </Modal.Content>
-            <ModalActions>
+            </ModalBody>
+            <ModalFooter>
                 <Button onClick={modalStore.closeModal}>OK</Button>
-            </ModalActions>
+            </ModalFooter>
         </Modal>
     )
 })
--- ficha-app/src/stores/modalStore.ts
+++ ficha-app/src/stores/modalStore.ts
@@ -1,24 +1,28 @@
+import { useDisclosure } from "@chakra-ui/react";
 import { makeAutoObservable } from "mobx"
 
 interface Modal {
     open : boolean;
     body: JSX.Element | null;
 }
+// const {onOpen} = useDisclosure();
 
 export default class ModalStore {
     modal : Modal = {
         open: false,
-        body: null
+        body: null,
+        
     }
 
     sucesso : boolean = false;
 
     constructor() {
         makeAutoObservable(this);
+        
     }
 
     openModal = (content : JSX.Element, comsucesso: boolean) => {
-        this.modal.open = true;
+        // this.modal.open = true;        
         this.modal.body = content;
         this.sucesso = comsucesso;
     }
